6aee5acfa2b3a70f32ccd72eab9efea849d29a5c
Björn Eberhardt <lazer.erazer@gmail.com>
2022-12-16 02:02:37 +0100

    test ytd-player elements for getPlayer() != null

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
 const script = document.createElement('script');
 script.innerText = `
+function getPlayerElement() {
+    var ytd_player = document.getElementsByTagName("ytd-player");
+    for (var player of ytd_player) {
+        if (player.getPlayer() && player.className.indexOf('preview') == -1) {
+            return player;
+        }
+    }
+}
 window.addEventListener('keydown', function (e) {
-  const player = document.querySelector('ytd-player').getPlayer();
+  const player = getPlayerElement().getPlayer();
   const seconds = 2;
   if (e.ctrlKey) {
     if (e.keyCode === 37) {
