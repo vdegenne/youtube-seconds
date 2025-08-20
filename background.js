498eed331cad82f81e32ecaff32b4d0d9c4680a2
Valentin Degenne <vdegenne@gmail.com>
2025-08-20 17:08:58 +0200

    0.2.0

---
+++ background.js
@@ -0,0 +1,1 @@
+chrome.runtime.onMessage.addListener((e,n,r)=>{});
--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,1 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	// if (message.action === 'openLichessTab') {
-	// 	// Query for the currently active tab
-	// 	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-	// 		const activeTab = tabs[0] // Get the active tab
-	// 		if (activeTab) {
-	// 			// Open a new tab next to the active tab
-	// 			chrome.tabs.create({
-	// 				url: 'https://lichess.org/paste',
-	// 				index: activeTab.index + 1, // Open it to the right of the active tab
-	// 			})
-	// 		}
-	// 	})
-	// }
-});
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {});
